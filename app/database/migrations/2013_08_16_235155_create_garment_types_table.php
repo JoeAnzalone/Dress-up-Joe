f09a1e5c9be085bf14e330043b201164e82f815b
Joe Anzalone <Joe@Shmit.com>
2013-08-16 19:54:32 -0400

    Create garment_types table migration, update foreign key column name in garments table migration

--- app/database/migrations/2013_08_16_235155_create_garment_types_table.php
+++ app/database/migrations/2013_08_16_235155_create_garment_types_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGarmentsTable extends Migration {
+class CreateGarmentTypesTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,12 +12,10 @@ class CreateGarmentsTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('garments', function(Blueprint $table)
+		Schema::create('garment_types', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->string('name');
-			$table->integer('garment_type_id')->unsigned();
-			$table->integer('user_id')->unsigned();
 			$table->timestamps();
 		});
 	}
@@ -29,7 +27,7 @@ class CreateGarmentsTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('garments');
+		Schema::drop('garment_types');
 	}
 
 }
