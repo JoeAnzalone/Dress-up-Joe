446ced9ca0495f183c56b9aa9d529f595ed6ec9e
Joe Anzalone <Joe@Shmit.com>
2013-08-16 19:49:58 -0400

    Create garments table migration

---
+++ app/database/migrations/2013_08_16_234657_create_garments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGarmentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('garments', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->integer('type_id')->unsigned();
+			$table->integer('user_id')->unsigned();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('garments');
+	}
+
+}
