6c763fa3ff443f7468a23ab43f536beb34ab7b0b
Joe Anzalone <Joe@Shmit.com>
2013-08-17 16:25:39 -0400

    Create default stylesheet

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>{{$title}}</title>
+    {{ HTML::style('css/main.css') }}
 </head>
 <body>
-
+<div class="page">
 {{ $content }}
-
+</div>
 </body>
 </html>
